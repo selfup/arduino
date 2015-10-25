2e95bff382b8e4f9f3763d6beb13c90205997f92
Regis Boudinot <selfup@users.noreply.github.com>
2015-10-24 23:16:50 -0600

    Update wow_loop_arduino.rb

--- wow_loop_arduino.rb
+++ wow_loop_arduino.rb
@@ -2,50 +2,59 @@ require 'rubygems'
 require 'arduino_firmata'
 require 'nokogiri'
 require 'open-uri'
-
-doc = Nokogiri::HTML(open("https://medium.com/@rboudinot"))
-
-arduino = ArduinoFirmata.connect  # use default arduino
-
+require 'pry'
+ 
+@doc = Nokogiri::HTML(open("http://selfup.me"))
+ 
+arduino = ArduinoFirmata.connect
+ 
 count = 0
-
+ 
 while count <= 50 do
-  if doc.text.include?("test 1 2 3")
-    puts "I found my article!"
+    if @doc.text.include?("Chris")                 
+    puts "I found Chris!"
     arduino.digital_write 13, true
-    sleep(3)
+    sleep(10)
     arduino.digital_write 13, false
-    sleep(1)
+    sleep(5)
     arduino.digital_write 13, true
-    sleep(3)
+    sleep(5)
     arduino.digital_write 13, false
-    sleep(1)
+    sleep(5)
+    arduino.digital_write 13, true
+    sleep(5)
+    arduino.digital_write 13, false
+    sleep(15)
     count = 0
+    @doc = Nokogiri::HTML(open("http://selfup.me"))
   elsif count == 3
-    sleep(1)
+    sleep(5)
     puts "Still waiting"
     count = 0
   else
+    sleep(30)                      
     arduino.digital_write 13, true
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, false
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, true
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, false
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, true
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, false
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, true
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, false
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, true
-    sleep(1)
+    sleep(0.2)
     arduino.digital_write 13, false
-    sleep(1)
+    sleep(0.2)
     count += 1
+    @doc = Nokogiri::HTML(open("http://selfup.me"))
+    sleep(5)
   end
 end
