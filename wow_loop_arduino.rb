2b2d8ea6c9d4f19f6b07211e4645181b560b2fbe
selfup <boudinot.regis@yahoo.com>
2015-10-24 18:25:51 -0600

    Live testing about to happen

--- wow_loop_arduino.rb
+++ wow_loop_arduino.rb
@@ -1,21 +1,51 @@
-require "rubygems"
-require "arduino_firmata"
+require 'rubygems'
+require 'arduino_firmata'
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open("https://medium.com/@rboudinot"))
 
 arduino = ArduinoFirmata.connect  # use default arduino
 
 count = 0
 
-while count <= 4 do
-  if count == 3
+while count <= 50 do
+  if doc.text.include?("test 1 2 3")
+    puts "I found my article!"
+    arduino.digital_write 13, true
+    sleep(3)
+    arduino.digital_write 13, false
     sleep(1)
-    puts "WOW"
+    arduino.digital_write 13, true
+    sleep(3)
+    arduino.digital_write 13, false
+    sleep(1)
+    count = 0
+  elsif count == 3
+    sleep(1)
+    puts "Still waiting"
     count = 0
   else
     arduino.digital_write 13, true
     sleep(1)
     arduino.digital_write 13, false
     sleep(1)
-
+    arduino.digital_write 13, true
+    sleep(1)
+    arduino.digital_write 13, false
+    sleep(1)
+    arduino.digital_write 13, true
+    sleep(1)
+    arduino.digital_write 13, false
+    sleep(1)
+    arduino.digital_write 13, true
+    sleep(1)
+    arduino.digital_write 13, false
+    sleep(1)
+    arduino.digital_write 13, true
+    sleep(1)
+    arduino.digital_write 13, false
+    sleep(1)
     count += 1
   end
 end
