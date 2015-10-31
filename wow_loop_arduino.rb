6828fcdb38602f1b5a942d55d8d4fb7cc975c376
selfup <boudinot.regis@yahoo.com>
2015-10-30 20:50:11 -0600

    reducing time between api calls

--- wow_loop_arduino.rb
+++ wow_loop_arduino.rb
@@ -45,64 +45,40 @@ while count <= 50 do
     arduino.digital_write 13, false
     sleep(0.5)
     arduino.digital_write 12, true
-    sleep(1)
+    sleep(0.5)
     arduino.digital_write 12, false
     sleep(0.5)
     arduino.digital_write 12, true
-    sleep(1)
+    sleep(0.5)
     arduino.digital_write 12, false
     sleep(0.5)
     arduino.digital_write 12, true
-    sleep(1)
+    sleep(0.5)
     arduino.digital_write 12, false
-    sleep(10)
+    sleep(1)
     count = 0
     still = 0
     puts "making sure"
     @pr = parse(connection.get("repos/#{user}/#{repo}/pulls")).count
   elsif count == 3
-    sleep(5)
+    sleep(1)
     puts "Still waiting"
     count = 0
     still += 1
     puts " This many times...#{still}"
   else
     if found >= 1
-      puts "\nI lost him!!!\nStill Looking for him!\n\n"
+      puts "\nPR closed!!!\nLooking for new open pull requests!\n\n"
     end
     arduino.digital_write 13, true
     sleep(4)
     arduino.digital_write 13, false
     sleep(4)
-    arduino.digital_write 13, true
-    sleep(4)
-    arduino.digital_write 13, false
-    sleep(4)
-    arduino.digital_write 13, true
-    sleep(4)
-    arduino.digital_write 13, false
-    sleep(4)
-    arduino.digital_write 13, true
-    sleep(4)
-    arduino.digital_write 13, false
-    sleep(4)
-    arduino.digital_write 13, true
-    sleep(4)
-    arduino.digital_write 13, false
-    sleep(4)
-    arduino.digital_write 13, true
-    sleep(1)
-    arduino.digital_write 13, false
-    sleep(1)
-    arduino.digital_write 13, true
-    sleep(1)
-    arduino.digital_write 13, false
     puts "fetching"
     sleep(1)
     count += 1
     @pr = parse(connection.get("repos/#{user}/#{repo}/pulls")).count
     sleep(1)
     puts "fetched! \n count = #{count} before 'Still Waiting'"
-    sleep(5)
   end
 end
