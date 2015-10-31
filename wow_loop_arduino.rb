bd36bbeb16dbc35398d1ce6a00676f8c2e4f6c5c
selfup <boudinot.regis@yahoo.com>
2015-10-31 14:53:59 -0600

    Now I have two LED's - green for when it finds - yellow for when it's fetching - and the buzzer is still in play for when a PRis found

--- wow_loop_arduino.rb
+++ wow_loop_arduino.rb
@@ -5,8 +5,8 @@ require 'hurley'
 require 'json'
 require 'pry'
 
-user = "selfup"
-repo = "arduino"
+user = "jphoenix86"
+repo = "melt_and_pourium"
 connection = Hurley::Client.new("https://api.github.com")
 connection.query[:access_token] = ENV["TOKEN"]
 connection.header[:accept] = "application/vnd.github+json"
@@ -15,73 +15,59 @@ def parse(response)
   JSON.parse(response.body)
 end
 
-# testing
-
-@pr = parse(connection.get("repos/#{user}/#{repo}/pulls")).count
+@pr ||= parse(connection.get("repos/#{user}/#{repo}/pulls")).count
 
 arduino = ArduinoFirmata.connect
 
 count = 0
 still = 0
 found = 0
-
-# testing github le hub
+light = 13
+sound = 12
+fetch = 8
 
 while count <= 50 do
   if @pr > 0
     found = 0
     found += 1
     puts "*****************  !!Open Pull Request!!  *********************"
-    arduino.digital_write 13, true
-    sleep(0.5)
-    arduino.digital_write 13, false
-    sleep(0.5)
-    arduino.digital_write 13, true
-    sleep(0.5)
-    arduino.digital_write 13, false
-    sleep(0.5)
-    arduino.digital_write 13, true
-    sleep(0.5)
-    arduino.digital_write 13, false
-    sleep(0.5)
-    arduino.digital_write 13, true
-    sleep(0.5)
-    arduino.digital_write 13, false
-    sleep(0.5)
-    arduino.digital_write 12, true
-    sleep(0.5)
-    arduino.digital_write 12, false
-    sleep(0.5)
-    arduino.digital_write 12, true
-    sleep(0.5)
-    arduino.digital_write 12, false
-    sleep(0.5)
-    arduino.digital_write 12, true
-    sleep(0.5)
-    arduino.digital_write 12, false
-    sleep(1)
+    5.times do
+      arduino.digital_write light, true
+      sleep(0.5)
+      arduino.digital_write light, false
+      sleep(0.5)
+    end
+    5.times do
+      arduino.digital_write sound, true
+      sleep(0.5)
+      arduino.digital_write sound, false
+      sleep(0.5)
+    end
     count = 0
     still = 0
     puts "making sure"
-    @pr = parse(connection.get("repos/#{user}/#{repo}/pulls")).count
+    arduino.digital_write fetch, true
+    sleep(1)
+    arduino.digital_write fetch, false
+    sleep(1)
+    @pr ||= parse(connection.get("repos/#{user}/#{repo}/pulls")).count
   elsif count == 3
     sleep(1)
-    puts "Still waiting"
+    puts "No Open PR's"
     count = 0
     still += 1
-    puts " This many times...#{still}"
+    puts " Fetched #{still * 3} times"
   else
     if found >= 1
       puts "\nPR closed!!!\nLooking for new open pull requests!\n\n"
     end
-    arduino.digital_write 13, true
-    sleep(4)
-    arduino.digital_write 13, false
-    sleep(4)
     puts "fetching"
+    arduino.digital_write fetch, true
+    sleep(1)
+    arduino.digital_write fetch, false
     sleep(1)
     count += 1
-    @pr = parse(connection.get("repos/#{user}/#{repo}/pulls")).count
+    @pr ||= parse(connection.get("repos/#{user}/#{repo}/pulls")).count
     sleep(1)
     puts "fetched! \n count = #{count} before 'Still Waiting'"
   end
