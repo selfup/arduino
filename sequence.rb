4242bea4f38b4fee3bb7368b4900ff80eeff119a
selfup <boudinot.regis@yahoo.com>
2015-10-31 21:10:37 -0600

    added a sequence module for morse code - dit - dah

---
+++ sequence.rb
@@ -0,0 +1,24 @@
+module Sequence
+
+  def dit(arduino)
+    3.times do
+      arduino.digital_write 13, true
+      arduino.analog_write 11, 2
+      sleep(0.2)
+      arduino.analog_write 11, 0
+      arduino.digital_write 13, false
+      sleep(0.2)
+    end
+  end
+
+  def dah(arduino)
+    3.times do
+      arduino.digital_write 13, true
+      arduino.analog_write 11, 2
+      sleep(0.6)
+      arduino.analog_write 11, 0
+      arduino.digital_write 13, false
+      sleep(0.2)
+    end
+  end
+end
